2d500c7c891f2045143361481e5d0b7f6a604fe6
Whitney Murray <wnksmith92@yahoo.com>
2015-11-16 16:55:05 -0600

    finished except for the Hi?

---
+++ main.js
@@ -0,0 +1,13 @@
+//declaring hello to save the info from the prompt, so I can use it later.
+//tried declaring hi, because it kept just saying nice to meet you and hi wasn't showing up. I know that's not correct.
+//console.log to add them all up together.
+
+var hello = prompt('What is your name\?');
+var hi=(hi);
+var greeting = (' \, nice to meet you\!');
+console.log(hi +hello+greeting);
+
+// I remembered what I was doing. I'm trying to do it without looking at anything. I want to be able to just do this stuff. Stupidest thing though I can't get Hi to show up I tried several different things? So stupid, I know.. I've done this sooo many times before I don't know what's wrong with me.
+
+
+
