230763e8921e32b1730a67f212b64729c7e0a01d
Whitney Murray <wnksmith92@yahoo.com>
2015-11-17 18:54:03 -0600

    finished

--- main.js
+++ main.js
@@ -3,10 +3,12 @@
 //console.log to add them all up together.
 
 var hello = prompt('What is your name\?');
-var hi=(hi);
-var greeting = (' \, nice to meet you\!');
-console.log(hi +hello+greeting);
+console.log(hello);
+var pre = ('Hello');
+var saying =(' '+'\, Nice to meet you\!');
 
+var greeting = pre + " " +hello + saying;
+console.log(greeting);
 // I remembered what I was doing. I'm trying to do it without looking at anything. I want to be able to just do this stuff. Stupidest thing though I can't get Hi to show up I tried several different things? So stupid, I know.. I've done this sooo many times before I don't know what's wrong with me.
 
 
