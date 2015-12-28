95a77b2baaa65c0674035108a71c26338289b680
Whitney Murray <wnksmith92@yahoo.com>
2015-12-28 13:33:53 -0600

    fixed spaceing issue

--- main.js
+++ main.js
@@ -5,9 +5,9 @@
 var hello = prompt('What is your name\?');
 console.log(hello);
 var pre = ('Hello');
-var saying =('\, Nice to meet you\!');
+var saying =('\,Nice to meet you\!');
 
-var greeting = pre + ' ' +hello + saying;
+var greeting = pre + ' ' +hello+saying;
 console.log(greeting);
 // I remembered what I was doing. I'm trying to do it without looking at anything. I want to be able to just do this stuff. Stupidest thing though I can't get Hi to show up I tried several different things? So stupid, I know.. I've done this sooo many times before I don't know what's wrong with me.
 
